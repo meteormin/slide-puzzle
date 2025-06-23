1b6cca90f51593d4f4f79c490eed9cd126b3f448
meteormin <miniyu97@gmail.com>
2025-06-23 19:49:35 +0900

    feat: wasm test

---
+++ ui/main.go
@@ -0,0 +1,24 @@
+//go:build js && wasm
+// +build js,wasm
+
+package main
+
+import (
+	"syscall/js"
+)
+
+func alertCallback(this js.Value, p []js.Value) interface{} {
+	js.Global().Call("alert", "버튼이 클릭되었습니다!")
+	return nil
+}
+
+func main() {
+	c := make(chan struct{}, 0)
+
+	// 버튼에 이벤트 핸들러 등록
+	document := js.Global().Get("document")
+	button := document.Call("getElementById", "myButton")
+	button.Call("addEventListener", "click", js.FuncOf(alertCallback))
+
+	<-c // 프로그램 종료 방지
+}
